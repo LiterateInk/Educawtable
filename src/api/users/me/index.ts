2b9dabee9585856502e4f8171c5632a23c2c82f8
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-13 22:21:54 +0200

    refactor: move types in its own file

--- src/api/users/me/index.ts
+++ src/api/users/me/index.ts
@@ -1,36 +1,5 @@
 import { EDUCARTABLE_API_EDUCORE } from "~/constants/url";
-
-interface ECApiUsersMe {
-  success: true
-  data: {
-    id: number
-    name: string
-    firstname: string
-    mail: string
-    activated: boolean
-    disabled: null
-    newsletter: boolean
-    com_avatar: null
-    com_name: null
-    pro_avatar: null
-    pro_name: null
-    teacher: boolean
-    parent: boolean
-    gender: null
-    birthday: null
-    last_cgu: null
-    keycloak_id: string
-    lang: null
-    allow_translation: null
-    group: string
-    roles: string[]
-  }
-}
-
-interface ApiUsersMe {
-  cookies: string[]
-  data: ECApiUsersMe["data"]
-}
+import type { ApiUsersMe, ECApiUsersMe } from "./types";
 
 export const callApiUsersMe = async (input: {
   token: string;
