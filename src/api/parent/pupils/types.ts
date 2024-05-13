c6df367227a38cd9a0a166ea06fdd9e94d7126ec
Tom Theret <tomnils.theret@gmail.com>
2024-05-13 19:42:49 +0200

    types(pupils): ApiParentPupilsRes

---
+++ src/api/parent/pupils/types.ts
@@ -0,0 +1,57 @@
+export interface ApiParentPupilsRes {
+  success: boolean;
+  data: Array<{
+    id: number;
+    name: string;
+    firstname: string;
+    birthday: string;
+    gender: string;
+    grade_id: number;
+    classroom_id: number;
+    transfering: boolean;
+    ine: any;
+    fullname: string;
+    writable: boolean;
+    grade: {
+      id: number;
+      letters: string;
+      name: string;
+    };
+    classroom: {
+      id: number;
+      name: string;
+      school_id: number;
+      release: boolean;
+      teacher_name: string;
+    };
+    parents: Array<{
+      id: number;
+      name: string;
+      firstname: string;
+      mail: string;
+      PupilsUser: {
+        id: number;
+        user_id: number;
+        pupil_id: number;
+        role: string;
+      };
+    }>;
+    groups: Array<{
+      id: number;
+      name: string;
+      PupilgroupsPupil: {
+        id: number;
+        pupil_id: number;
+        pupilgroup_id: number;
+      };
+    }>;
+  }>;
+  pagination: {
+    page_count: number;
+    current_page: number;
+    has_next_page: boolean;
+    has_prev_page: boolean;
+    count: number;
+    limit: number;
+  };
+}
