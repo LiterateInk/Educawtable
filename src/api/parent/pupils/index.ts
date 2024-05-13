b391ed34ea12c03bfd4f04e2dd85696503588342
Tom Theret <tomnils.theret@gmail.com>
2024-05-13 19:43:11 +0200

    feat(pupils): Implement Pupils list

---
+++ src/api/parent/pupils/index.ts
@@ -0,0 +1,28 @@
+import { EDUCARTABLE_API_EDUCORE } from "~/constants/url";
+import type { ApiParentPupilsRes } from "./types";
+
+export const callApiParentPupils = async (input: {
+  /** Auth */
+  token: string;
+  session: string;
+  /** Request parameters */
+  id: number;
+}): Promise<ApiParentPupilsRes> => {
+  const response = await fetch(`${EDUCARTABLE_API_EDUCORE}/parent/${input.id}/pupils`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: input.token,
+      Cookie: input.session
+    }
+  });
+
+  const json = (await response.json()) as ApiParentPupilsRes;
+
+  return {
+    success: json.success,
+    data: json.data,
+    pagination: json.pagination
+  };
+};
