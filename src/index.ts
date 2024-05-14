faf012863e275ed001b9e8b404d1a1354e88a87c
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-14 23:59:26 +0200

    chore: exports classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-export { default as authenticate } from "~/helpers/authenticate";
+export { default as authenticate, type AuthenticationParameters } from "~/helpers/authenticate";
 
-export { default as defaultFetcher } from "~/helpers/default-fetcher";
 export type { Fetcher } from "~/helpers/types/fetcher";
+export { default as defaultFetcher } from "~/helpers/default-fetcher";
+
+export { default as Family } from "~/models/Family";
+
+export type { ApiUser } from "~/constants/types/user";
+export type { ApiTokens } from "~/constants/types/tokens";
