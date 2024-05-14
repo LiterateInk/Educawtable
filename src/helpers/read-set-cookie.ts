569a84668939ac78d65540cfd6a579d40cca76af
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-14 18:55:58 +0200

    refactor: to `readSetCookie`

--- src/helpers/read-set-cookie.ts
+++ src/helpers/read-set-cookie.ts
@@ -1,7 +1,7 @@
 import cookieParser from "set-cookie-parser";
 import readHeaders from "~/helpers/read-headers";
 
-export const retrieveResponseCookies = (headers: Record<string, string> | Headers): string[] => {
+export default function readSetCookie (headers: Record<string, string> | Headers): string[] {
   const setCookieHeader = readHeaders(headers, "set-cookie");
   if (setCookieHeader === null) return [];
 
