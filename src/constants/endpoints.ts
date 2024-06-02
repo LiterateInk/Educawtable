f9dba9069123814bc76d3dd6af209fb47ac00299
Tom Theret <tomnils.theret@gmail.com>
2024-06-02 15:24:23 +0200

    endpoint: Add RPC url

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,4 +1,5 @@
 export const EDUCARTABLE_API_EDUCORE = "https://mobile.educartable.com/api/1.0/educore";
 export const EDUCARTABLE_API_EDUCARTABLE = "https://app.educartable.com/api/1.0/educartable";
 export const EDUCARTABLE_API_CORE = "https://app.educartable.com/api/1.0/core";
+export const EDUCARTABLE_RPC = "https://app.educartable.com/rpc";
 export const EDUMOOV_ACCOUNTS_REALM = "https://accounts.edumoov.com/auth/realms/edumoov";
